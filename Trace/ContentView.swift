bc8151b30da36876964b6f3839f7156301932dbe
Cauldronpass <david@familyweiss.net>
2026-06-14 09:51:51 -0500

    Initial Commit

---
+++ Trace/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Trace
+//
+//  Created by David Weiss on 6/14/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Trace/TraceApp.swift
@@ -0,0 +1,17 @@
+//
+//  TraceApp.swift
+//  Trace
+//
+//  Created by David Weiss on 6/14/26.
+//
+
+import SwiftUI
+
+@main
+struct TraceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
